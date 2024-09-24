a9a117597f423466095b87ec31a681dac72082c1
Pranav-Panchal <pranavkpanchal4@gmail.com>
2024-09-24 14:13:21 -0400

    new commit

---
+++ Week4/lab3/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 4</title>
+</head>
+<body>
+    <h1>Registration Form</h1>
+    <form action="">
+        <label for="">Username</label>
+        <input type="text" name="username" id="username" placeholder="Enter your username">
+        <label for="">Email</label>
+        <input type="email" name="email" id="email" placeholder="Enter your email">
+        <label for="">Password</label>
+        <input type="text" name="password" id="password" placeholder="Enter your password">        
+    </form>
+</body>
+</html>
